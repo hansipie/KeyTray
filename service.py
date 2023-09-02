76559d57a16b25aeb046fdf1df590d0f44b2d360
Hansi P <hansipie@users.noreply.github.com>
2023-09-02 18:34:33 +0300

    windows service capabilities

---
+++ service.py
@@ -0,0 +1,39 @@
+import win32serviceutil
+import win32service
+import win32event
+import servicemanager
+import socket
+import sys
+import App
+
+class MyService(win32serviceutil.ServiceFramework):
+    _svc_name_ = 'KeyTray'
+    _svc_display_name_ = 'KeyTray'
+    _svc_description_ = 'Displays the status of the NumLock key in the system tray'
+
+    def __init__(self, args):
+        win32serviceutil.ServiceFramework.__init__(self, args)
+        self.hWaitStop = win32event.CreateEvent(None, 0, 0, None)
+        socket.setdefaulttimeout(60)
+
+    def SvcStop(self):
+        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+        win32event.SetEvent(self.hWaitStop)
+
+    def SvcDoRun(self):
+        servicemanager.LogMsg(servicemanager.EVENTLOG_INFORMATION_TYPE,
+                              servicemanager.PYS_SERVICE_STARTED,
+                              (self._svc_name_, ''))
+        self.main()
+
+    def main(self):
+        App.main()
+        pass
+
+if __name__ == '__main__':
+    if len(sys.argv) == 1:
+        servicemanager.Initialize()
+        servicemanager.PrepareToHostSingle(MyService)
+        servicemanager.StartServiceCtrlDispatcher()
+    else:
+        win32serviceutil.HandleCommandLine(MyService)
